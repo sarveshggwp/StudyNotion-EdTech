c0f21953dda2b5921388d5334221f881780f240f
sarveshggwp <sarveshkochhar1@gmail.com>
2024-12-20 00:49:26 +0530

    gg1

--- src/data/navbar-links.js
+++ src/data/navbar-links.js
@@ -5,7 +5,17 @@ export const NavbarLinks = [
   },
   {
     title: "Catalog",
-    // path: '/catalog',
+    dropdown: [
+      { title: "Web Development", path: "/catalog/web-development" },
+      { title: "Python", path: "/catalog/python" },
+      { title: "Web Design", path: "/catalog/web-design" },
+      { title: "Artificial Intelligence", path: "/catalog/artificial-intelligence" },
+      { title: "DevOps", path: "/catalog/devops" },
+      { title: "Blockchain Technology", path: "/catalog/blockchain-technology" },
+      { title: "Cloud Computing", path: "/catalog/cloud-computing" },
+      { title: "Generative AI", path: "/catalog/generative-ai" },
+      { title: "Brian Scott", path: "/catalog/brian-scott" },
+    ],
   },
   {
     title: "About Us",
